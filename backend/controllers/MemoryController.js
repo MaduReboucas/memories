8d7189d33f64fedb8c1e7277eb3cc41c1a23d549
Duda Rebouças <mariadudareboucas@gmail.com>
2026-03-30 15:57:16 -0300

    Resgatando todas as memórias

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -1,9 +1,40 @@
 const Memory = require("../models/Memory");
 
 const createMemory = async (req, res) => {
-  res.json("Deu certo! - memory controller");
+  try {
+    const { title, description } = req.body;
+
+    const src = `images/${req.file.filename}`;
+
+    if (!title || description) {
+      return res.status(400).json({ mag: "Por favor, preencha todos campos." });
+    }
+
+    const newMemory = new Memory({
+      title,
+      src,
+      description,
+    });
+
+    await newMemory.save();
+
+    res.json({ msg: "Memória criada com sucesso!", newMemmory });
+  } catch (error) {
+    console.log(error.mensage);
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
+
+const getMemories = async (req, res) => {
+  try {
+    const memories = await Memory.find();
+    res.json(memories);
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
 };
 
 module.exports = {
   createMemory,
+  getMemories,
 };
--- backend/routes.js
+++ backend/routes.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createMemory } = require("./controllers/MemoryController");
+const upload = require("./helpers/upload");
 
-router.post("/", (req, res) => createMemory(req, res));
+const { createMemory, getMemories } = require("./controllers/MemoryController");
+
+router.post(
+  "/",
+  upload.single("image"),
+  (req, res, next) => {
+    const image = req.file;
+
+    if (!image) {
+      return res.status(400).json({ mag: "Por favor, envie um arquivo." });
+    }
+    next();
+  },
+  (req, res) => createMemory(req, res),
+);
+
+router.get("/", (req, res) => getMemories(req, res));
 
 module.exports = router;
