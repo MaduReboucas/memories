5857ea17ff43094a3f5b055377ca2c6eb67a59d5
Duda Rebouças <mariadudareboucas@gmail.com>
2026-03-30 17:35:41 -0300

    Favoritando memórias

--- backend/routes.js
+++ backend/routes.js
@@ -10,6 +10,7 @@ const {
   getMemory,
   deleteMemory,
   updateMemory,
+  toggleFavorite,
 } = require("./controllers/MemoryController");
 
 router.post(
@@ -35,5 +36,7 @@ router.delete("/:id", (req, res) => deleteMemory(req, res));
 router.patch("/:id", upload.single("image"), (req, res) =>
   updateMemory(req, res),
 );
+
+router.patch("/favorite/:id", (req, res) => toggleFavorite(req, res));
 
 module.exports = router;
