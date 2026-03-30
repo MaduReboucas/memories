a17cc3182801f5edf63ef83d12dbe2a0098ea0cf
Duda Rebouças <mariadudareboucas@gmail.com>
2026-03-30 19:27:11 -0300

    Criando barra de navegação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 
 import { Outlet } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Memories</h1>
       <div className="container">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
