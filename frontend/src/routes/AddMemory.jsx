72d85b9451281e6dcc885f4af49088ad6d8cf300
Duda Rebouças <mariadudareboucas@gmail.com>
2026-03-31 14:59:36 -0300

    Criando formulário

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,7 +1,34 @@
 import "./AddMemory.css";
 
+import axios from "axios";
+
+import { useState } from "react";
+
 const AddMemory = () => {
-  return <div>AddMemory</div>;
+  return (
+    <div className="add-memory-page">
+      <h2>Crie uma memória</h2>
+      <form>
+        <label>
+          <p>Título:</p>
+          <input type="text" name="title" placeholder="Difina um título" />
+        </label>
+        <label>
+          <p>Descrição:</p>
+          <textarea
+            type="text"
+            name="description"
+            placeholder="Explique o que aconteceu"
+          ></textarea>
+        </label>
+        <label>
+          <p>Foto:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" value="Enviar" className="btn" />
+      </form>
+    </div>
+  );
 };
 
 export default AddMemory;
